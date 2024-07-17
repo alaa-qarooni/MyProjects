862116b6b8e5f479413d97f3cf75a263048a9423
alaa-qarooni <alaa.qarooni.96@gmail.com>
2024-07-17 12:12:55 -0400

    added simulation files

---
+++ Animation/NN.py
@@ -0,0 +1,154 @@
+import pdb
+import random
+import numpy as np
+import math
+
+import random
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+# if GPU is to be used
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
+
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+    
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 256)
+        self.layer2 = nn.Linear(256, 256)
+        self.layer3 = nn.Linear(256, 256)
+        self.layer4 = nn.Linear(256, 256)
+        self.layer5 = nn.Linear(256, n_actions)
+        self.dropout = nn.Dropout(p=0.2)  # Dropout with 20% probability
+
+    def forward(self, x):
+        x = F.normalize(x,dim=0)
+        x = F.relu(self.layer1(x))
+        x = self.dropout(x)
+        x = F.relu(self.layer2(x))
+        x = self.dropout(x)
+        x = F.relu(self.layer3(x))
+        x = self.dropout(x)
+        x = F.relu(self.layer4(x))
+        x = self.layer5(x)
+        return x
+
+class simulator:
+    # BATCH_SIZE is the number of transitions sampled from the replay buffer
+    # GAMMA is the discount factor
+    # EPS_START is the starting value of epsilon
+    # EPS_END is the final value of epsilon
+    # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+    # TAU is the update rate of the target network
+    # LR is the learning rate of the ``AdamW`` optimizer
+
+    ### VARY THESE TO SEE HOW PERFORMANCE IMPROVES ###
+
+    def __init__(self, states, actions):
+        self.BATCH_SIZE = 128
+        self.GAMMA = 0.90
+        self.EPS_START = 0.9
+        self.EPS_END = 0.05
+        self.EPS_DECAY = 20000
+        self.TAU = 0.05
+        self.LR = 1e-5
+
+        # Store action space
+        self.actions = actions
+        # Store state space
+        self.states = states
+
+        self.policy_net = DQN(len(states), len(actions)).to(device)
+        self.target_net = DQN(len(states), len(actions)).to(device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.LR, amsgrad=True)
+        self.memory = ReplayMemory(10000)
+        
+        self.steps_done = 0
+
+
+    def select_action(self, state):
+        global steps_done
+        sample = random.random()
+        eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * math.exp(-1. * self.steps_done / self.EPS_DECAY)
+        self.steps_done += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                # t.max(0) will return the largest value. Index indicates which action
+                # returns highest reward.
+                a = self.policy_net(state.to(device)).max(0).indices.view(1)
+                return torch.tensor([a],device=device,dtype=torch.long)
+        else:
+            return torch.tensor([random.randint(0,len(self.actions)-1)], device=device, dtype=torch.long)
+
+
+    def optimize_model(self):
+        if len(self.memory) < self.BATCH_SIZE:
+            return
+        transitions = self.memory.sample(self.BATCH_SIZE)
+        # Transpose the batch
+        batch = Transition(*zip(*transitions))
+        del transitions
+
+        # Gather next states
+        next_states = torch.stack(batch.next_state).to(device)
+        
+        state_batch = torch.stack(batch.state).to(device)
+        action_batch = torch.cat(batch.action).unsqueeze(0).to(device)
+        reward_batch = torch.cat(batch.reward).unsqueeze(0).to(device)
+
+        # Compute Q function values (taken from NN)
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for next_states are computed based on the "older"
+        # target_net; selecting their best reward with max(1).values
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        with torch.no_grad():
+            next_state_values = self.target_net(next_states).max(1).values
+        
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.GAMMA) + reward_batch
+
+        # Compute Huber loss
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values)
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
---
+++ Animation/Simulation.py
@@ -0,0 +1,226 @@
+import Space
+import NN
+
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import numpy as np
+
+import os
+import math
+import torch
+import itertools
+import pickle
+from collections import deque
+
+import pymunk
+from pymunk.vec2d import Vec2d
+
+def train_model(dt, n_episodes=10,episode_length=10):
+    
+    # state is composed of the positions and velocities of N-1 nearest dynamic balls
+    # REALTIVE TO 1 kinematic ball.
+    # The kinematic balls will take the action from the target network.
+    # Action space is direction kinematic ball can take: 0 rad - 2pi rad
+    N = 11
+    action_space = list(itertools.product(np.arange(-3,4,0.5),np.arange(-3,3.1,0.5)))
+    state_space = [0]*N*4
+    model = NN.simulator(state_space,action_space)
+
+    for ep in range(n_episodes):
+        width,height,space = Space.initialize()
+
+        running_r = torch.tensor([0.])
+        
+        balls = space.bodies[:-1]
+        our_guy = space.bodies[-1]
+
+        prev_vel = tuple(our_guy.velocity)
+
+        s = get_state(N,space.bodies)
+        for t in np.arange(0,episode_length,dt):
+            # select action (between no change if no balls around, and NN otherwise)
+            a = model.select_action(s)
+
+            # Apply action to velocity
+            a_vel, a_ang = model.actions[a]
+            x_vel = a_vel*math.cos(a_ang)
+            y_vel = a_vel*math.sin(a_ang)
+
+            our_guy.velocity = x_vel,y_vel
+
+            # Step in simulation
+            space.step(dt)
+
+            collision_count = 0
+            # Check if ball hits the wall too many times (repetitive collision state)
+            if our_guy.position.x < 0.6 or our_guy.position.x > width - 0.6 or our_guy.position.y < 0.6 or our_guy.position.y > height - 0.6:
+                collision_count +=1
+                r = torch.tensor([-50*dt])
+            elif any([Vec2d.get_distance(our_guy.position,b.position) < our_guy.radius + b.radius + 0.1 for b in balls]):
+                r = torch.tensor([-50*dt])
+            else:
+                # Get next state
+                s_prime = get_state(N,space.bodies)
+                r = torch.tensor([dt])
+
+            running_r += r
+
+            # Store transition in memory
+            model.memory.push(s,a,s_prime,r)
+
+            # Move to the next state
+            s = s_prime
+
+            # Perform one step of optimization
+            model.optimize_model()
+
+            # Soft update of target weights
+            target_net_state_dict = model.target_net.state_dict()
+            policy_net_state_dict = model.policy_net.state_dict()
+            for key in policy_net_state_dict:
+                target_net_state_dict[key] = policy_net_state_dict[key]*model.TAU + target_net_state_dict[key]*(1-model.TAU)
+            model.target_net.load_state_dict(target_net_state_dict)
+
+            if collision_count == 5:
+                print("Repetitive wall collisions")
+                break
+
+        # Survival time in seconds
+        print(f"Kin ball {ep} received {round(running_r.item(),2)} reward")
+
+    return model
+
+# State is defined by the relative positions and velocities of N nearby balls
+def get_state(N, bodies):
+    #Get nearest N balls
+    balls = sorted(bodies,key=lambda b: Vec2d.get_distance(bodies[-1].position,b.position))[:N]
+    
+    # Make kinetic ball features the last to follow convention (sorted func above made it first)
+    balls[0], balls[-1] = balls[-1], balls[0]
+
+    # Get positions and velocities
+    s = [(*tuple(b.position),*tuple(b.velocity)) for b in balls]
+
+    # unpack into one vector
+    s = list(itertools.chain(*s))
+
+    return torch.tensor(s ,dtype=torch.float32).to(NN.device)
+
+
+# Function to flip the velocity when it hits the boundary
+def flip_velocity_if_boundary(width,height,object):
+    # Double if statements ensure - or + velocity when corresponding boundary is hit
+    if object.position.x > width - object.radius - 0.1:
+        object.velocity = -1*abs(object.velocity.x), object.velocity.y
+    elif object.position.x < object.radius + 0.1:
+        object.velocity = abs(object.velocity.x), object.velocity.y
+
+    if object.position.y > height - object.radius - 0.1:
+        object.velocity = object.velocity.x, -1*abs(object.velocity.y)
+    elif object.position.y < object.radius + 0.1:
+        object.velocity = object.velocity.x, abs(object.velocity.y)
+
+def sim(space, T, dt, model):
+    ts = np.arange(0,T,dt)
+    bodies = space.bodies
+    frame_info = [[tuple(b.position) for b in bodies]]
+
+    prev_vel = tuple(bodies[-1].velocity)
+
+    for t in ts:
+        
+        # get state of N nearest balls
+        s = get_state(11, bodies)
+
+        # If nearby balls, get action from model, otherwise no velocity change
+        if any([Vec2d.get_distance(bodies[-1].position,b.position)<bodies[-1].radius+b.radius+0.5 for b in bodies[:-1]]):
+            a_v,a_ang = model["actions"][model["network"](s).max(0).indices.view(1)]
+
+            # Set velocity values
+            x_vel = a_v*math.cos(a_ang)
+            y_vel = a_v*math.sin(a_ang)
+
+            # Apply exponential smoothing to dampen the new velocity's effect
+            smth_f = 0.7
+            x_vel = smth_f*x_vel + (1-smth_f)*prev_vel[0]
+            y_vel = smth_f*y_vel + (1-smth_f)*prev_vel[1]
+            
+            prev_vel = (x_vel,y_vel)
+
+            bodies[-1].velocity = x_vel, y_vel
+        
+        flip_velocity_if_boundary(width,height,bodies[-1])
+
+        #Step the simulation - KEY
+        space.step(dt)
+
+        #log ball positions
+        frame_info.append([tuple(b.position) for b in bodies])
+    
+    return ts[: len(frame_info)], frame_info
+
+if __name__ == "__main__":
+    T = 50 # How long to simulate
+    dt = 1/150 # we simulate 60 timesteps per second
+    
+    # Train and save the target network, and set of actions to choose from
+    if not os.path.isfile("Exercises/Animation/model.pt"):
+        simulator = train_model(dt)
+        
+        network = torch.jit.script(simulator.target_net)
+        network.save("Exercises/Animation/model.pt")
+
+        with open("Exercises/Animation/actions", "wb") as path:
+            pickle.dump(simulator.actions, path)
+    
+    # Load the network and set of actions to use in the simulation
+    model = {}
+    model["network"] = torch.jit.load("Exercises/Animation/model.pt")
+    with open("Exercises/Animation/actions", "rb") as path:
+        model["actions"] = pickle.load(path)
+    
+    width , height, space = Space.initialize()
+    ts, frame_info = sim(
+        space, T, dt, model
+    )
+
+    bodies = space.bodies
+
+    #Subsamping: render one of 10 below subsamples
+    subsampling = 10
+
+    # Prepare figure and axes
+    fig, ax = plt.subplots(figsize=(width,height))
+    ax.set(xlim=[0,width], ylim=[0,height])
+    ax.set_aspect("equal")
+    ax.set_position([0,0,1,1])
+    fig.set(facecolor="y")
+
+    #Prepare the patches for the balls
+    cmap = plt.get_cmap("twilight")
+    circles = [plt.Circle((0,0), radius=b.radius, facecolor=cmap(i/len(bodies))) for i,b in enumerate(bodies[:-1])]
+    our_guy = plt.Circle((0,0),radius=bodies[-1].radius, facecolor = "Green")
+    for c in circles:
+        ax.add_patch(c)
+        ax.add_patch(our_guy)
+
+    #Draw walls
+    for s in space.static_body.shapes:
+        ax.plot([s.a.x,s.b.x], [s.a.y,s.b.y], linewidth=2, color="k")
+
+    #animation function. Called for each frame, passing an entry in positions
+    def drawframe(p):
+        for i, c in enumerate (circles):
+            c.set_center(p[i])
+        our_guy.set_center(p[-1])
+        return circles + [our_guy]
+    
+    anim = animation.FuncAnimation(
+        fig,
+        drawframe,
+        frames=frame_info[::subsampling],
+        interval = dt * subsampling * 1000,
+        blit=True
+    )
+    
+    plt.show()
